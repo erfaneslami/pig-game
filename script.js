43355bd6131b3c3d9fd533a6bda2d621042cb791
erfan eslami <erfan.88.eslami@gmail.com>
2021-12-24 02:40:03 +0330

    add dice code

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ const btnNew = document.querySelector(".btn--new");
 // INITIAL STATE
 score0El.textContent = 0;
 score1El.textContent = 0;
-diceEl.classList.add("hidden");
+// diceEl.classList.add("hidden");
 const totalScore = [0, 0];
 let currentScore = 0;
 let activePlayer = 0;
@@ -35,8 +35,14 @@ btnRollDice.addEventListener("click", function () {
   if (runing) {
     // CALC DICE AND SHOW TO DICE TO SCREEN
     const diceRoll = Math.trunc(Math.random() * 6) + 1;
-    diceEl.classList.remove("hidden");
-    diceEl.src = `dice-${diceRoll}.png`;
+    const faceDice = document.querySelectorAll("[class^='face']");
+    for (let i = 0; i < faceDice.length; i++) {
+      faceDice[i].classList.add("hidden");
+    }
+
+    document.querySelector(`.face-${diceRoll}`).classList.remove("hidden");
+    // diceEl.classList.remove("hidden");
+    // diceEl.src = `dice-${diceRoll}.png`;
 
     // CHECK IF IT NOT ONE :
     if (diceRoll !== 1) {
