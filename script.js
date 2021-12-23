81b0202b4af251be2835e4da1509e0b73eab4bd8
erfan eslami <erfan.88.eslami@gmail.com>
2021-12-23 23:45:59 +0330

    add roll dice btn functionality

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
-'use strict';
+"use strict";
+
+// ELEMENTS VARIABLES
+const player0El = document.querySelector(".player--0");
+const player1El = document.querySelector(".player--1");
+const score0El = document.getElementById("score--0");
+const score1El = document.getElementById("score--1");
+const current0El = document.getElementById("current--0");
+const current1El = document.getElementById("current--1");
+const diceEl = document.querySelector(".dice");
+const btnRollDice = document.querySelector(".btn--roll");
+const btnHold = document.querySelector(".btn--hold");
+
+// INITIAL STATE
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add("hidden");
+const totalScore = [0, 0];
+let currentScore = 0;
+let activePlayer = 0;
+
+btnRollDice.addEventListener("click", function () {
+  const diceRoll = Math.trunc(Math.random() * 6) + 1;
+  diceEl.classList.remove("hidden");
+  diceEl.src = `dice-${diceRoll}.png`;
+
+  if (diceRoll !== 1) {
+    currentScore += diceRoll;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
+  } else {
+    currentScore = 0;
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    player0El.classList.toggle("player--active");
+    player1El.classList.toggle("player--active");
+  }
+});
